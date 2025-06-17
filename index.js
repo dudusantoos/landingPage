1cfdc8096acc5adaed4ad225c2febb59361956bc
Eduardo <edu.salmeida@gmail.com>
2025-06-17 07:15:21 -0300

    V1.0

---
+++ index.js
@@ -0,0 +1,19 @@
+var setaEsquerda = window.document.getElementById("setaEsquerda")
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+var setaDireita = window.document.getElementById("setaDireita")
+
+function rolarDireita() {
+    leonardo.style = "display:none"
+    bruna.style = "display:flex"
+    setaDireita.style = "display:none"
+    setaEsquerda.style = "display:flex"
+}
+
+function rolarEsquerda() {
+    leonardo.style = "display:flex"
+    bruna.style = "display:none"
+    setaDireita.style = "display:flex"
+    setaEsquerda.style = "display:none"
+}
